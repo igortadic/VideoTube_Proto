dea98412e9023fe6b48cd784caafa1dfa6c8b33d
TehStud <igortadic.msc@gmail.com>
2022-06-06 09:07:26 +0200

    Init commit v0
    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>VideoTube</title>
+  <link rel="stylesheet" Type="text/css" href="assets/css/style.css">
+</head>
+<body>
+  <div id="pageContainer">
+      <div id="mastHeadContainer">
+        hello
+      </div>
+      <div id="sideNavContainer">
+
+      </div>
+      <div id="mainSectionContainer">
+
+      </div>
+  </div>
+</body>
+</html>
